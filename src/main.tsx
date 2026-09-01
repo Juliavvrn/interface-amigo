873630a26f750de115985ef28c6e0160c8e523ca
Juliavvrn <160138043+Juliavvrn@users.noreply.github.com>
2026-09-02 00:18:43 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "@tanstack/react-router";
-import { createRouter } from "./router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { getRouter } from "./router";
 import "./styles.css";
 
-const router = createRouter();
+const queryClient = new QueryClient();
+const router = getRouter();
 
 const rootElement = document.getElementById("root")!;
 if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+      </QueryClientProvider>
     </React.StrictMode>
   );
 }
